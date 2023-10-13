61ec0b25a2b3555b251c3bf0caa990ff59bdc2d7
Daniel Egoavil <daniel.programmer@hotmail.com>
2023-10-12 22:17:45 -0500

    Funcion de sign in y logout termiando

--- app/Http/Controllers/admin/AuthController.php
+++ app/Http/Controllers/admin/AuthController.php
@@ -28,17 +28,12 @@ class AuthController extends Controller
     function Login(Request $R){
         $user = User::where('email', $R->email)->first();
 
-        if($user!='[]' && Hash::check($R->password,$user->password)){
+        if($user && Hash::check($R->password,$user->password)){
             $token = $user->createToken('Personal Access Token')->plainTextToken;
             $response = ['status' => 200, 'token' => $token, 'user' => $user, 'message' => 'Bienvenido!'];
-            return response()->json($response);
-        }else if($user=='[]'){
-            $response = ['status' => 500, 'message' => 'Usuario no encontrado'];
-            return response()->json($response);
-
         }else{
             $response = ['status' => 500, 'message' => 'Email o contraseña incorrecta'];
-            return response()->json($response);
         }
+        return response()->json($response);
     }
 }
