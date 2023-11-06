ac569aeaf7dfdf40b6e969c1799d0d160ec35644
Daniel Egoavil <daniel.programmer@hotmail.com>
2023-11-05 21:40:05 -0500

    agregar mantenimiento, editado y terminado

--- app/Http/Controllers/admin/MaintenanceController.php
+++ app/Http/Controllers/admin/MaintenanceController.php
@@ -21,33 +21,33 @@ class MaintenanceController extends Controller
                 'required',
                 Rule::in(['Operativo', 'Operativo con fallas', 'No operativo']),
             ],
-            'soplado_general' => 'required|boolean',
-            'ventiladores' => 'required|boolean',
-            'disipador_del_procesador' => 'required|boolean',
-            'ranuras_de_expansion' => 'required|boolean',
-            'tarjetas_de_memoria' => 'required|boolean',
-            'fuente_de_poder' => 'required|boolean',
-            'lectora_de_CD_DVD' => 'required|boolean',
-            'monitor' => 'required|boolean',
-            'teclado' => 'required|boolean',
-            'mouse' => 'required|boolean',
-            'desfragmentado_de_disco_Scandisk' => 'required|boolean',
-            'mantenimiento_de_archivos' => 'required|boolean',
-            'asistente_para_quitar_programas' => 'required|boolean',
-            'eliminacion_de_archivos_temporales' => 'required|boolean',
-            'eliminacion_de_cookies_y_archivos_temporales' => 'required|boolean',
-            'analisis_con_antivirus' => 'required|boolean',
-            'antiSpyware' => 'required|boolean',
-            'analisis_de_registro' => 'required|boolean',
-            'prueba_de_impresion_antes_del_mantenimiento' => 'required|boolean',
-            'impresion_pagina_de_configuracion' => 'required|boolean',
-            'verificacion_del_contador_de_paginas_impresas' => 'required|boolean',
-            'verificacion_del_estado_de_los_consumibles' => 'required|boolean',
-            'limpieza_de_los_consumibles' => 'required|boolean',
-            'verificacion_de_los_mecanismos_de_impresion' => 'required|boolean',
-            'limpieza_de_los_mecanismos_de_impresion' => 'required|boolean',
-            'limpieza_externa_del_equipo' => 'required|boolean',
-            'prueba_de_impresion_despues_del_mantenimiento' => 'required|boolean'
+            'soplado_general' => 'boolean',
+            'ventiladores' => 'boolean',
+            'disipador_del_procesador' => 'boolean',
+            'ranuras_de_expansion' => 'boolean',
+            'tarjetas_de_memoria' => 'boolean',
+            'fuente_de_poder' => 'boolean',
+            'lectora_de_CD_DVD' => 'boolean',
+            'monitor' => 'boolean',
+            'teclado' => 'boolean',
+            'mouse' => 'boolean',
+            'desfragmentado_de_disco_Scandisk' => 'boolean',
+            'mantenimiento_de_archivos' => 'boolean',
+            'asistente_para_quitar_programas' => 'boolean',
+            'eliminacion_de_archivos_temporales' => 'boolean',
+            'eliminacion_de_cookies_y_archivos_temporales' => 'boolean',
+            'analisis_con_antivirus' => 'boolean',
+            'antiSpyware' => 'boolean',
+            'analisis_de_registro' => 'boolean',
+            'prueba_de_impresion_antes_del_mantenimiento' => 'boolean',
+            'impresion_pagina_de_configuracion' => 'boolean',
+            'verificacion_del_contador_de_paginas_impresas' => 'boolean',
+            'verificacion_del_estado_de_los_consumibles' => 'boolean',
+            'limpieza_de_los_consumibles' => 'boolean',
+            'verificacion_de_los_mecanismos_de_impresion' => 'boolean',
+            'limpieza_de_los_mecanismos_de_impresion' => 'boolean',
+            'limpieza_externa_del_equipo' => 'boolean',
+            'prueba_de_impresion_despues_del_mantenimiento' => 'boolean'
         ]);
 
         if ($validator->fails()) {
@@ -58,39 +58,48 @@ class MaintenanceController extends Controller
         }
 
         $loggedInUser = Auth::user()->name;
-
         $maintenance = new Maintenance();
+        $maintenance->fill($request->all());
+        // Establecer valores por defecto para los campos booleanos si no están presentes en la solicitud
+        $booleanFields = [
+            'soplado_general',
+            'ventiladores',
+            'disipador_del_procesador',
+            'ranuras_de_expansion',
+            'tarjetas_de_memoria',
+            'fuente_de_poder',
+            'lectora_de_CD_DVD',
+            'monitor',
+            'teclado',
+            'mouse',
+            'desfragmentado_de_disco_scandisk',
+            'mantenimiento_de_archivos',
+            'asistente_para_quitar_programas',
+            'eliminacion_de_archivos_temporales',
+            'eliminacion_de_cookies_y_archivos_temporales',
+            'analisis_con_antivirus',
+            'antiSpyware',
+            'analisis_de_registro',
+            'prueba_de_impresion_antes_del_mantenimiento',
+            'impresion_pagina_de_configuracion',
+            'verificacion_del_contador_de_paginas_impresas',
+            'verificacion_del_estado_de_los_consumibles',
+            'limpieza_de_los_consumibles',
+            'verificacion_de_los_mecanismos_de_impresion',
+            'limpieza_de_los_mecanismos_de_impresion',
+            'limpieza_externa_del_equipo',
+            'prueba_de_impresion_despues_del_mantenimiento'
+        ];
+
+        foreach ($booleanFields as $field) {
+            if (!$maintenance->isDirty($field)) {
+                $maintenance->{$field} = false;
+            }
+        }
+
         $maintenance->name_user = $loggedInUser;
         $maintenance->code_device = $request->code_device;
         $maintenance->observations = $request->observations;
-        $maintenance->status = $request->status;
-        $maintenance->soplado_general = $request->soplado_general;
-        $maintenance->ventiladores = $request->ventiladores;
-        $maintenance->disipador_del_procesador = $request->disipador_del_procesador;
-        $maintenance->ranuras_de_expansion = $request->ranuras_de_expansion;
-        $maintenance->tarjetas_de_memoria = $request->tarjetas_de_memoria;
-        $maintenance->fuente_de_poder = $request->fuente_de_poder;
-        $maintenance->lectora_de_CD_DVD = $request->lectora_de_CD_DVD;
-        $maintenance->monitor = $request->monitor;
-        $maintenance->teclado = $request->teclado;
-        $maintenance->mouse = $request->mouse;
-        $maintenance->desfragmentado_de_disco = $request->desfragmentado_de_disco;
-        $maintenance->scandisk = $request->scandisk;
-        $maintenance->mantenimiento_de_archivos = $request->mantenimiento_de_archivos;
-        $maintenance->asistente_para_quitar_programas = $request->asistente_para_quitar_programas;
-        $maintenance->eliminacion_de_archivos_temporales = $request->eliminacion_de_archivos_temporales;
-        $maintenance->eliminacion_de_cookies_y_archivos_temporales = $request->eliminacion_de_cookies_y_archivos_temporales;
-        $maintenance->analisis_con_antivirus_antiSpyware = $request->analisis_con_antivirus_antiSpyware;
-        $maintenance->analisis_de_registro = $request->analisis_de_registro;
-        $maintenance->prueba_de_impresion_antes_del_mantenimiento = $request->prueba_de_impresion_antes_del_mantenimiento;
-        $maintenance->impresion_pagina_de_configuracion = $request->impresion_pagina_de_configuracion;
-        $maintenance->verificacion_del_contador_de_paginas_impresas = $request->verificacion_del_contador_de_paginas_impresas;
-        $maintenance->verificacion_del_estado_de_los_consumibles = $request->verificacion_del_estado_de_los_consumibles;
-        $maintenance->limpieza_de_los_consumibles = $request->limpieza_de_los_consumibles;
-        $maintenance->verificacion_de_los_mecanismos_de_impresion = $request->verificacion_de_los_mecanismos_de_impresion;
-        $maintenance->limpieza_de_los_mecanismos_de_impresion = $request->limpieza_de_los_mecanismos_de_impresion;
-        $maintenance->limpieza_externa_del_equipo = $request->limpieza_externa_del_equipo;
-        $maintenance->prueba_de_impresion_despues_del_mantenimiento = $request->prueba_de_impresion_despues_del_mantenimiento;
         $maintenance->save();
 
         return response()->json([
@@ -138,13 +147,13 @@ class MaintenanceController extends Controller
                 'monitor',
                 'teclado',
                 'mouse',
-                'desfragmentado_de_disco',
-                'scandisk',
+                'desfragmentado_de_disco_scandisk',
                 'mantenimiento_de_archivos',
                 'asistente_para_quitar_programas',
                 'eliminacion_de_archivos_temporales',
                 'eliminacion_de_cookies_y_archivos_temporales',
-                'analisis_con_antivirus_antiSpyware',
+                'analisis_con_antivirus',
+                'antiSpyware',
                 'analisis_de_registro',
                 'prueba_de_impresion_antes_del_mantenimiento',
                 'impresion_pagina_de_configuracion',
@@ -176,13 +185,13 @@ class MaintenanceController extends Controller
                 'monitor' => 'required|boolean',
                 'teclado' => 'required|boolean',
                 'mouse' => 'required|boolean',
-                'desfragmentado_de_disco' => 'required|boolean',
-                'scandisk' => 'required|boolean',
+                'desfragmentado_de_disco_scandisk' => 'required|boolean',
                 'mantenimiento_de_archivos' => 'required|boolean',
                 'asistente_para_quitar_programas' => 'required|boolean',
                 'eliminacion_de_archivos_temporales' => 'required|boolean',
                 'eliminacion_de_cookies_y_archivos_temporales' => 'required|boolean',
-                'analisis_con_antivirus_antiSpyware' => 'required|boolean',
+                'analisis_con_antivirus' => 'required|boolean',
+                'antiSpyware'=> 'required|boolean',
                 'analisis_de_registro' => 'required|boolean',
                 'prueba_de_impresion_antes_del_mantenimiento' => 'required|boolean',
                 'impresion_pagina_de_configuracion' => 'required|boolean',
